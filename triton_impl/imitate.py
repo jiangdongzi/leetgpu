7f12c24158b1ead28342900106d00bcd58f70bf7
jiangdongzi <jiangdongzidi@126.com>
2026-04-16 08:02:25 +0000

    stash

--- triton_impl/imitate.py
+++ triton_impl/imitate.py
@@ -15,17 +15,15 @@ def matrix_multiplication_kernel(
     GROUP_SIZE_M: tl.constexpr
 ):
     pid = tl.program_id(axis=0)
-    
     num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
     num_pid_k = tl.cdiv(K, BLOCK_SIZE_K)
-
-    num_pid_in_group = num_pid_k * GROUP_SIZE_M;
+    num_pid_in_group = GROUP_SIZE_M * num_pid_k
     group_id = pid // num_pid_in_group
     first_pid_m = group_id * GROUP_SIZE_M
     group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
-
-    pid_m = first_pid_m + (pid % num_pid_in_group) % group_size_m
-    pid_k = (pid % num_pid_in_group) // group_size_m
+    local_pid_num = pid % num_pid_in_group
+    pid_m = first_pid_m + local_pid_num % group_size_m
+    pid_k = local_pid_num // group_size_m
 
     offs_m = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_k = pid_k * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
@@ -34,27 +32,20 @@ def matrix_multiplication_kernel(
     msk_m = offs_m < M
     msk_k = offs_k < K
 
-    aptrs = a_ptr + (offs_m[:, None] * stride_am + offs_n[None, :] * stride_an)
-    bptrs = b_ptr + (offs_n[:, None] * stride_bn + offs_k[None, :] * stride_bk)
-
     acc = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_K), dtype=tl.float32)
 
+    aptrs = offs_m[:, None] * stride_am + offs_n[None, :] * stride_an + a_ptr
+    bptrs = offs_n[:, None] * stride_bn + offs_k[None, :] * stride_bk + b_ptr
+
     for n in range(0, tl.cdiv(N, BLOCK_SIZE_N)):
-        offs_n_cur = n * BLOCK_SIZE_N + offs_n
-        msk_n = offs_n_cur < N
+        offs_n_curr = n * BLOCK_SIZE_N + offs_n
+        msk_n = offs_n_curr < N
 
-        a = tl.load(aptrs, mask=msk_m[:, None] & msk_n[None, :], other=0.0)
-        b = tl.load(bptrs, mask=msk_n[:, None] & msk_k[None, :], other=0.0)
-
+        a = tl.load(aptrs, msk_m[:, None] & msk_n[None, :], other=0.0)
+        b = tl.load(bptrs, msk_n[:, None] & msk_k[None, :], other=0.0)
         acc += tl.dot(a, b)
-
         aptrs += BLOCK_SIZE_N * stride_an
         bptrs += BLOCK_SIZE_N * stride_bn
-    
-    cptrs = c_ptr + (offs_m[: None] * stride_cm + offs_k[None, :] * stride_ck)
-
-    tl.store(cptrs, acc, mask=msk_m[: None] & msk_k[None, :])
-
     pass
 
 def solve(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, M: int, N: int, K: int):
@@ -68,7 +59,7 @@ def solve(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, M: int, N: int, K: 
     GROUP_SIZE_M = 8
 
     grid = lambda META: (
-        triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(K, META["BLOCK_SIZE_K"]),
+        triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(K, META['BLOCK_SIZE_K']),
     )
 
     matrix_multiplication_kernel[grid](
